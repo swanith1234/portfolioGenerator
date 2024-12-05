5e7e4ff9a57e8dae07414fadbe82288fc2f60bb4
Afroze <mohammadafroze26@gmail.com>
2024-12-05 20:23:58 +0530

    added new

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Ensure the output folder is correctly set
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[hash][extname]", // CSS files will go under `assets/`
+        chunkFileNames: "assets/[name].[hash].js", // JS chunks
+        entryFileNames: "assets/[name].[hash].js",
+      },
+    },
+  },
+});
