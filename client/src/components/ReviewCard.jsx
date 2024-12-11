9ae95107224d00ca7738e8b03e5f8daf5716a7b1
swanith1234 <swanithpidugu@gmail.com>
2024-12-11 19:26:42 +0530

    added the frontend developed by afroze

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import star from '../assets/star.svg'
+import star from "../assets/star.svg";
 
-const ReviewCard = ({imgURL,customerName,rating,feedback}) => {
+const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
   return (
-     <>
-     <div className="flex justify-center items-center flex-col ">
-        <img src={imgURL} alt="customer"
-        className='rounded-full object-cover w-[120px] h-[120px] ' />
-        <p className='mt-6 max-w-sm text-center info-text'>{feedback}</p>
-        <div className='mt-3 flex justify-center items-center gap-2.5  '> 
-            <img src={star}
-            width={24}
-            height={24} alt="star"
-            className='object-contain m-0' />
-            <p className='text-xl font-montserrat text-slate-gray '>({rating})</p>
-        </div>
-        <h3 className='mt-1 font-palanquin text-3xl text-center font-bold '>{customerName}</h3>
-    </div>
-     
-     </>
-  )
-}
+    <>
+      <div className="flex justify-center items-center flex-col ">
+        <img
+          src={imgURL}
+          alt="customer"
+          className="rounded-full object-cover w-[120px] h-[120px] "
+        />
+        <h3 className="mt-1 font-palanquin text-3xl text-center font-bold ">
+          {customerName}
+        </h3>
+        <img
+          src={star}
+          width={24}
+          height={24}
+          alt="star"
+          className="object-contain m-0"
+        />
+        <p className="text-xl font-montserrat text-slate-gray ">({rating})</p>
+        <p className="mt-6 max-w-sm text-center info-text">{feedback}</p>
+        <div className="mt-3 flex justify-center items-center gap-2.5  "></div>
+      </div>
+    </>
+  );
+};
 
-export default ReviewCard
+export default ReviewCard;
--- client/src/sections/CustomerReviews.jsx
+++ client/src/sections/CustomerReviews.jsx
@@ -1,51 +1,53 @@
-import React from 'react';
-import ReviewCard from '../components/ReviewCard';
-import customer1 from '../assets/customer1.jpeg';
-import customer2 from '../assets/customer2.svg';
+import React from "react";
+import ReviewCard from "../components/ReviewCard";
+import customer1 from "../assets/customer1.jpeg";
+import customer2 from "../assets/customer2.svg";
 
-
-  const reviews = [
-    {
-        imgURL: customer1,
-        customerName: 'Morich Brown',
-        rating: 4.5,
-        feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
-    },
-    {
-        imgURL: customer2,
-        customerName: 'Lota Mongeskar',
-        rating: 4.5,
-        feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
-    }
+const reviews = [
+  {
+    imgURL: customer1,
+    customerName: "Morich Brown",
+    rating: 4.5,
+    feedback:
+      "Using this portfolio generator was a game-changer for me! I was able to create a professional portfolio in no time.",
+  },
+  {
+    imgURL: customer2,
+    customerName: "Lota Mongeskar",
+    rating: 4.5,
+    feedback:
+      "The templates are beautifully designed and so easy to customize. My portfolio stands out thanks to this tool!",
+  },
 ];
 
 const CustomerReviews = () => {
   return (
-     <>
-       <section className='max-container py-8'>
-      <h3 className='font-palanquin text-center text-4xl font-bold'>What Our
-        <span className='text-amber-600'> Customers </span>
-        Say?
-      </h3>
-      <p className='info-text m-auto  mt-4 max-w-lg text-center '>Hear genuine stories from our
-        satisfied customers about their
-        exceptional experiences with us.</p>
+    <>
+      <section className="max-container py-8">
+        <h3 className="font-palanquin text-center text-4xl font-bold">
+          What Our
+          <span className="text-amber-600"> Customers </span>
+          Say?
+        </h3>
+        <p className="info-text m-auto  mt-4 max-w-lg text-center ">
+          Hear genuine stories from our satisfied customers about their
+          exceptional experiences with us.
+        </p>
 
         <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
-          {reviews.map((review)=>(
+          {reviews.map((review) => (
             <ReviewCard
-             key={review.customerName} imgURL = {review.imgURL}
-             customerName = {review.customerName}
-             rating = {review.rating}
-             feedback = {review.feedback}
+              key={review.customerName}
+              imgURL={review.imgURL}
+              customerName={review.customerName}
+              rating={review.rating}
+              feedback={review.feedback}
             />
           ))}
         </div>
-    </section>
+      </section>
+    </>
+  );
+};
 
-
-     </>
-  )
-}
-
-export default CustomerReviews
+export default CustomerReviews;
