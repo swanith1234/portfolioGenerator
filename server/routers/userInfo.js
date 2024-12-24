471b3c4cd1988d958554442dfe1c1ec900430231
swanith kumar reddy pidugu <146310162+swanith1234@users.noreply.github.com>
2024-12-24 12:43:37 +0530

    Update userInfo.js

--- server/routers/userInfo.js
+++ server/routers/userInfo.js
@@ -2,5 +2,6 @@ import express from "express";
 import { createUserInfo } from "../controllers/userInfoController.js";
 
 const router = express.Router();
+console.log("swanith");
 router.post("/post/userInfo", createUserInfo);
 export default router;
