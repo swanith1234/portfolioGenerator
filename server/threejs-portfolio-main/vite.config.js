d06758f29c205059ed6bc76f494f614ddff5a8bd
swanith kumar reddy pidugu <146310162+swanith1234@users.noreply.github.com>
2024-12-30 17:47:23 +0530

    Update vite.config.js

--- server/threejs-portfolio-main/vite.config.js
+++ server/threejs-portfolio-main/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
+  },
+  build: {
+    sourcemap: false,  // Disable sourcemaps for build
   },
 });
